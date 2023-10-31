e4b28cc9b025abfb54f92d4010623e0a5e4a1b54
BRANDON <brandonturcios@unitec.edu>
2023-10-30 18:56:28 -0600

    funciona

--- myapp/app.js
+++ myapp/app.js
@@ -1,19 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mongoose = require("mongoose");
+ require("dotenv").config();
+app.use(express.json());
 
-app.get('/', (req, res) => {
+mongoose.connect(process.env.MONGOOSE_KEY, {
+  useNewUrlParser: true,
+});
+
+app.get('/', (req, res) => 
+{
+  console.log(process.env.MONGOOSE_KEY);
   res.send('Hello World!')
 })
-
-app.get('/a', (req, res) => {
-    res.send('LA LETRA A!')
+app.get('/hola2', (req, res) => {
+    res.send('Hello World!')
   })
-
-app.get('/b', (req, res) => {
-    res.send('LA LETRA B!')
+  
+  app.get('/hola', (req, res) => {
+    res.send('Hello World!')
   })
-
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
